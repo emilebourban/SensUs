e78fc60893fbcaae50f535ac54d903e1581a09a9
rcucu <raluca-maria.cucu@epfl.ch>
2019-08-13 23:32:30 +0200

    Update testlight.py

--- testlight.py
+++ testlight.py
@@ -2,15 +2,9 @@
 
 from light import Light
 from time import sleep
-from gpzero import LED
-
-led = LED(6)
-led.on()
 
 if __name__ == '__main__':
 	l = Light(4, 0.0, 0.5)
 	l.on()
 	sleep(1000)
-	l.off()
-
-led.off()	
+	l.off()	
