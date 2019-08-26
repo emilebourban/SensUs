e6aaa3fef5e07cd22c08f03b7d25d5d49b7e827c
rcucu <raluca-maria.cucu@epfl.ch>
2019-08-26 12:28:21 +0200

    push

--- application/acquisition/__init__.py
+++ application/acquisition/__init__.py
@@ -68,7 +68,7 @@ class Capture(Acquistion):
         self.log.debug(f'image converted: {image_converted}')
         image.Release()
         self.log.debug(f'image released: {image_converted}')
-        return image_converted
+        return image_converted.getNDArray()
 
     def get_exposure_time(self):
         self.EndAcquisition()
