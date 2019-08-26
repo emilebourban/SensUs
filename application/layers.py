e8c7175f761006bcc1325da72ecb70fa8b92d4be
VianneyRousset <6491145+VianneyRousset@users.noreply.github.com>
2019-08-26 19:38:23 +0200

    bug fixed

--- application/layers.py
+++ application/layers.py
@@ -224,7 +224,7 @@ class LoadingLayer(Layer):
                                    lambda: self.set_circles([]))
         self['size'] = gui.Slider(self, (400, 480 - 50), (350, 64), 10, 200,
                                   lambda r: self.set_selected_circles_radius(r))
-        self['progress'] = gui.Loading_bar(self, (400, 100), (400, 10),
+        self['progress'] = gui.Loading_bar(self, (400, 80), (300, 8),
                                            progress=0.3)
 
     def select_circle(self, c):
