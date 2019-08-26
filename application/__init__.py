ab62bdc0f39d8749f56a4baea83f997b74abeb81
VianneyRousset <6491145+VianneyRousset@users.noreply.github.com>
2019-08-26 15:28:39 +0200

    add mutlithreading

--- application/__init__.py
+++ application/__init__.py
@@ -2,15 +2,13 @@
 
 from . import gui
 import os
-import numpy as np
 from . import layers
 #from . import image_analysis
-from . import acquisition
+from . import ifconfig
+from . import photographer
 import pygame
 from logging import getLogger
 from time import time
-from subprocess import run, PIPE
-import re
 
 # TODO set fullscreen=True at gui.init
 
@@ -23,6 +21,7 @@ class Application(dict):
         self.debug = debug
         self.is_raspi = is_raspi
         self.screen = gui.init(fullscreen=is_raspi, hide_cursor=False)
+        self.photographer = photographer.Photographer()
         super().__init__({
             'welcome': layers.WelcomeLayer(self),
             'main': layers.MainLayer(self),
@@ -45,14 +44,6 @@ class Application(dict):
         self.quitting = False
         self.draw_fps = draw_fps
         self.ip_refresh_time = ip_refresh_time
-        self.live_fps = live_fps
-        self.capture_refresh_time = capture_refresh_time
-        self.acq = None
-        self.acq_i = 0
-        self.acquisition_mode = None
-        self.live_image = None
-        self.result_path = 'results/img_'
-        self.n_results = 10
 
     @property
     def active_layer(self):
@@ -69,48 +60,13 @@ class Application(dict):
         if self._active_layer == 'loading':
             self.acquisition_mode = 'capture'
 
-    @property
-    def acquisition_mode(self):
-        return self._acquisition_mode
-
-    @acquisition_mode.setter
-    def acquisition_mode(self, m):
-        if m not in ('capture', 'live_stream', None):
-            raise KeyError(m)
-        self.log.debug(f'Setting acquisition mode to "{m}"')
-        self._acquisition_mode = m
-
-        if m == 'capture':
-            del self.acq
-            self.acq = acquisition.Capture()
-            self.expo_time = self.acq.get_exposure_time()
-            self.log.info(f'New expo time: {self.expo_time}us')
-            del self.acq
-            self.acq = acquisition.LiveStream()
-            self.acq_i = 0
-        if m is not None:
-            del self.acq
-            self.acq = acquisition.LiveStream()
-
     def run(self):
+        self.photographer.start()
         self.quitting = False
         t_draw = time()
         t_ip = time()
-        t_live = time()
-        t_capt = time()
 
         while not self.quitting:
-
-            # livestream
-            if self.acquisition_mode and time() - t_live > 1/self.live_fps:
-                t_live = time()
-                self.live_image = self.acq.get_image()
-
-            # capture
-            if self.acquisition_mode == 'capture' \
-                    and time() - t_capt > self.capture_refresh_time:
-                t_capt = time()
-                self.capture()
 
             # events
             self.exec_events()
@@ -118,41 +74,16 @@ class Application(dict):
             # update ip
             if self.debug and time() - t_ip > self.ip_refresh_time:
                 t_ip = time()
-                ips = self.get_ip_addresses()
-                self.over_layer['ip'].text = ' - '.join(ips)
+                self.over_layer['ip'].text = ifconfig.get_ip_addresses_str()
 
             # drawing
             if time() - t_draw >= 1 / self.draw_fps:
                 t_draw = time()
                 self.draw()
 
+        self.photographer.stop()
+        self.photographer.join(5)
         return True
-
-    def capture(self):
-        del self.acq
-        self.acq = acquisition.Capture(expo_time=self.expo_time)
-        img = self.acq.get_image()
-        print('>> 1 >> ', img)
-        path = self.result_path + f"{self.acq_i:04d}"
-        np.save(path, img)
-        self.log.debug(f'Capture to "{path}"')
-        self.acq_i += 1
-        if self.acq_i >= self.n_results:
-            self.acquisition_mode = 'live_stream'
-        del self.acq
-        self.acq = acquisition.LiveStream()
-        print('>> 2 >> ', img)
-
-    def get_ip_addresses(self):
-        try:
-            pat = re.compile('^(\w+):.*$\n\s*inet\s+(\d+\.\d+\.\d+.\d+)',
-                             re.MULTILINE)
-            cmd = run(['ifconfig'], stdout=PIPE, encoding='utf8').stdout
-            return [f"{m.group(1)}: {m.group(2)}" for m in pat.finditer(cmd)]
-        except BaseException as e:
-            self.log.warn(f'Failed to get ip addresses: {e}')
-            return ['Failed to get IP addresses']
-
 
     def exec_events(self):
         for event in pygame.event.get():
---
+++ application/photographer.py
@@ -0,0 +1,102 @@
+from threading import Thread, Empty, Full
+from queue import Queue, Event
+from time import time
+from logging import getLogger
+from . import acquisition
+import numpy as np
+
+
+class Photographer(Thread):
+
+    # !! FROM THE OUTSIDE, ONLY CALL start(), set_mode(...), get_live_image()
+    # !! and stop())
+
+    def __init__(self, capture_path='results/img_', n_acquisitions=10,
+                 live_stream_fps=24, capture_refresh_time=30):
+        self.log = getLogger('main.Photographer')
+        self.log.debug('Photographer created')
+        self.mode_queue = Queue()
+        self.live_image_queue = Queue(8)
+        self.live_stream_fps = live_stream_fps
+        self.capture_refresh_time = capture_refresh_time
+        self.capture_path = capture_path
+        self.n_acquisitions = 10
+        self.acquisition = None
+        self.acquisition_i = 0
+        self.mode = None
+        self.quitting = Event()
+
+    def set_mode(self, m):
+        if m not in (None, 'live_stream', 'capture'):
+            raise KeyError(m)
+        self.mode_queue.put(m)
+
+    def get_live_image(self):
+        return self.live_image_queue.get()
+
+    def run(self):
+        self.log.debug('Photographer start')
+        t_live = time()
+        t_capt = time()
+
+        while not self.quitting.is_set():
+
+            # get new mode
+            if not self.mode_queue.empty():
+                try:
+                    self._set_mode(self.mode_queue.get(block=False))
+                    self.log.debug(f'Mode set to {self.mode}')
+                except Empty:
+                    pass
+
+            # capture
+            if self.mode == 'capture' \
+                    and time() - t_capt > self.capture_refresh_time:
+                t_capt = time()
+                self.capture()
+
+            # livestream
+            if self.mode \
+                    and time() - t_live > 1 / self.live_stream_fps:
+                t_live = time()
+                live_image = self.acquisition.get_image()
+                try:
+                    self.live_image.put(live_image)
+                except Full:
+                    self.log.warn('Live stream frame drop, (queue is full)')
+
+    def stop(self):
+        self.quitting.set()
+
+    def capture(self):
+        del self.acquisition
+        self.acquisition = acquisition.Capture(expo_time=self.expo_time)
+        img = self.acquisition.get_image()
+        path = self.result_path + f"{self.acquisition_i:04d}"
+        np.save(path, img)
+        self.log.debug(f'Capture to "{path}"')
+        self.acquisition_i += 1
+        if self.acquisition_i >= self.n_acquisitions:
+            self.log.info('Capture mode ended')
+            self.acquisition_mode = 'live_stream'
+        del self.acquisition
+        self.acquisition = acquisition.LiveStream()
+
+    def _set_mode(self, m):
+        if m not in ('capture', 'live_stream', None):
+            raise KeyError(m)
+        self.log.debug(f'Setting acquisition mode to "{m}"')
+        self.mode = m
+
+        if m == 'capture':
+            del self.acquisition
+            self.acquisition = acquisition.Capture()
+            self.expo_time = self.acquisition.get_exposure_time()
+            self.log.info(f'New expo time: {self.expo_time}us')
+            del self.acquisition
+            self.acquisition = acquisition.LiveStream()
+            self.acquisition_i = 0
+
+        if m == 'live_stream':
+            del self.acquisition
+            self.acquisition = acquisition.LiveStream()
