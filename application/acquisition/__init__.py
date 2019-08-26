ab823c03707304c1c593d2fd41e98253b1d3eb83
VianneyRousset <6491145+VianneyRousset@users.noreply.github.com>
2019-08-26 18:11:15 +0200

    bug fixed

--- application/acquisition/__init__.py
+++ application/acquisition/__init__.py
@@ -42,7 +42,7 @@ class Capture(Acquistion):
         self.log.debug('created capture')
         self.cam['StreamBufferHandlingMode'].value = 'NewestOnly'
         # TODO: use full depth, i.e 12 bits for image analysis :PixelFormat_Mono12p, try with packed
-        self.cam['PixelFormat'].value = 'Mono8'
+        self.cam['PixelFormat'].value = 'Mono12'
         self.cam['AcquisitionMode'].value = 'Continuous'
         self.cam['StreamCRCCheckEnable'].value = True
 
@@ -66,7 +66,7 @@ class Capture(Acquistion):
 
             # Convert image to Mono8
         import PySpin as spin
-        image_converted = image.Convert(spin.PixelFormat_Raw8)
+        image_converted = image.Convert(spin.PixelFormat_Mono16)
         self.log.debug(f'image converted: {image_converted}')
         image.Release()
         self.log.debug(f'image released: {image_converted}')
